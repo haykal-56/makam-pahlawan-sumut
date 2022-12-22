9df16953161ff5ad513c00ff8da6d6129f45b8ce
haykal-56 <78428555+haykal-56@users.noreply.github.com>
2022-12-22 17:58:52 +0700

    Add files via upload

---
+++ zainul.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kelompok 7</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" 
+    rel="stylesheet"
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" 
+    crossorigin="anonymous">
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" 
+    crossorigin="anonymous"></script>
+
+
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
+    integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
+    crossorigin=""/>
+    
+  <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
+    integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM="
+    crossorigin=""></script>
+  </head>
+  <style>
+    #map{
+      height: 80vh;
+    }
+  </style>
+<body>
+    <nav class="navbar navbar-expand-lg bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand text-light" href="#">Makam Pahlawan</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active text-light" aria-current="page" href="index.php">Home</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Daftar Pahlawan
+                </a>
+                <ul class="dropdown-menu">
+                <li><a class="dropdown-item text-dark" href="sisingamangaraja.php">1. Sisingamangaraja XII</a></li>
+                <li><a class="dropdown-item text-dark" href="tengku.php">2. Tengku Amir Hamzah</a></li>
+                <li><a class="dropdown-item text-dark" href="ferdinand.php">3. Dr. Ferdinand Lumban Tobing</a></li>
+                <li><a class="dropdown-item text-dark" href="zainul.php">4. K. H. Zainul Arifin</a></li>
+                <li><a class="dropdown-item text-dark" href="pandjaitan.php">5. Mayjen TNI Anm. D. I. Pandjaitan</a></li>
+              </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+    </nav>
+    <br>
+    <div class="text-center">
+        <img src="https://sumutprov.go.id/images/untukmasyarakat/ZainulArifin.jpg" width="250" height="250" alt="...">
+    </div>
+    <br>
+    <div class="container">
+      <h3 class="col-sm-4" >Sejarah singkat</h3>
+      <p class="col-sm-12">
+          Ia dikenal sebagai ulama, politis, pejuang 1945, Panglima Hizbullah, dan ketua DPR-GR (1960-1963). Zainul Arifin lahir sebagai anak tunggal dari pasangan keturunan raja Barus, Sultan Ramali bin Tuangku Raja Barus Sultan Sahi Alam Pohan (ayah) dengan bangsawan asal Kotanopan, Mandailing Natal, Siti Baiyah Nasution. Ketika Zainul masih balita, kedua orang tuanya bercerai dan ia dibawa pindah oleh ibunya ke Kotanopan, kemudian ke Kerinci, Jambi. Disana ia menyelesaikan Hollands Indische School (HIS) dan sekolah menengah calon guru, Normal School. 
+      </p>
+      <p class="col-sm-12">
+          Zainul Arifin memperdalam pengetahuan agama di madrasah dan surau saat menjalani pelatihan seni bela diri pencak silat. Ia juga seorang pecinta kesenian yang aktif dalam kegiatan seni sandiwara musikal Melayu, Stambul Bangsawan sebagai penyanyi dan pemain biola. Stambul Bangsawan merupakan awal perkembangan seni panggung sandiwara modern Indonesia. Dalam usia 16 tahun, Zainul merantau ke Batavia (Jakarta).
+      </p>
+      <p class="col-sm-12">
+          Di tengah meningkatnya suhu politik, pada 14 Mei 1962, saat shalat Idul Adha di barisan terdepan bersama Sukarno, Zainul tertembak peluru yang diarahkan seorang pemberontak DI/TII dalam percobaannya membunuh presiden. Zainul Arifin wafat tanggal 2 Maret 1963 setelah menderita luka bekas tembakan dibahunya selama sepuluh bulan.
+      </p>
+      <h3 class="col-sm-4" >Lokasi pemakaman</h3>
+      <p class="">
+          Ia dimakamkan di Taman Makam Pahlawan (TMP) Kalibata, Jakarta.
+      </p>
+      <div id="map"></div>
+    </div>
+    <script>
+    var map = L.map('map').setView([-6.256917, 106.845412], 15);
+    var marker = L.marker([-6.256917, 106.845412]).addTo(map);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+</script>
+</body>
+</html>
